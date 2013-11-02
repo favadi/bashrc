a455f8e2f8b2955c818ae076fd170e4d7879ebea
Diep Pham Van <imeo@favadi.com>
2013-11-02 18:37:58 +0700

    Fix autocd for bash 3

--- bash_opt.sh
+++ bash_opt.sh
@@ -2,8 +2,10 @@
 # Bash Options: ~/.bash/bash_opt.sh
 #
 
-# Auto "cd" when entering just a path 
-shopt -s autocd
+# Auto "cd" when entering just a path (only for bash 4)
+if [[ "$BASH_VERSINFO" -ge 4 ]]; then
+    shopt -s autocd
+fi
 
 # History
 export HISTSIZE=2000
