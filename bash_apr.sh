4c3d488f01fc392eea24661279f47153bdf5ca7a
Diep Pham Van <imeo@favadi.com>
2014-04-04 09:46:09 +0700

    Change PS1 promt

--- bash_apr.sh
+++ bash_apr.sh
@@ -6,10 +6,10 @@
 not_on_server
 if [[ $? -eq 0 ]]; then
     # Not on server, host name is blue
-    PS1='\e[0;32m\u\e[m \e[0;37mat\e[m \e[0;34m\h\e[m \e[0;33m\w\e[m\n\$ '
+    PS1='\e[0;32m\u\e[m at \e[0;34m\h\e[m \e[0;33m\w\e[m\n\$ '
 else
     # On server, host name is red
-    PS1='\e[0;32m\u\e[m \e[0;37mat\e[m \e[0;31m\h\e[m \e[0;33m\w\e[m\n\$ '
+    PS1='\e[0;32m\u\e[m at \e[0;31m\h\e[m \e[0;33m\w\e[m\n\$ '
 fi
 
 if [[ "$TERM" = 'dumb' ]]; then
